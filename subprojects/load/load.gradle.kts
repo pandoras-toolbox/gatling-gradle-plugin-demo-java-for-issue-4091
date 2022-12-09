2fb8fc9e03ed98679ef2dba00c596004907ca298
Zeljko Markovic <zeljko.markovic@adcubum.com>
2022-12-09 17:08:29 +0100

    Configure Gatling in Gradle

--- subprojects/load/load.gradle.kts
+++ subprojects/load/load.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     `java-test-fixtures`
 }
 
+// Commented because of:
+// Cannot access 'io.gatling.gradle.JvmConfigurable.Trait.FieldHelper' which is a supertype of 'io.gatling.gradle.GatlingPluginExtension'. Check your module classpath for missing or conflicting dependencies
+//gatling {
+//    // WARNING: options below only work when logback config file isn't provided
+//    logLevel = "WARN" // logback root level
+//    logHttp = "NONE" // set to 'ALL' for all HTTP traffic in TRACE, 'FAILURES' for failed HTTP traffic in DEBUG
+//}
+
 dependencies {
     gatlingImplementation(project(":common"))
     gatlingImplementation(testFixtures(project(":common")))
